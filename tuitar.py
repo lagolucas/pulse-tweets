277415088cf08f6a88a89c5d66cd39ed734afcbf
Lucas Lago <lago@nic.br>
2020-09-14 21:52:00 -0300

    alterações para reduzir o tamanho do FIO

--- tuitar.py
+++ tuitar.py
@@ -7,21 +7,28 @@ if __name__ == '__main__':
     languages = ['pt', 'en']
 
     for language in languages:
-        first_tweet = TweetBotParams.select().where((TweetBotParams.slug == 'start') 
-            & (TweetBotParams.lang == language) 
-            & (TweetBotParams.period_day == sys.argv[1])).get()
-        previous_status = twitter.start_thread(first_tweet)
+        first = True
+        previous_status = None
+
+        # first_tweet = TweetBotParams.select().where((TweetBotParams.slug == 'start') 
+        #     & (TweetBotParams.lang == language) 
+        #     & (TweetBotParams.period_day == sys.argv[1])).get()
+        # previous_status = twitter.start_thread(first_tweet)
 
         status_list = TweetBotQueue.select(TweetBotQueue, TweetBotParams).join(TweetBotParams).where(
             (TweetBotQueue.param.lang == language) 
             & (TweetBotParams.period_day == sys.argv[1]) 
-            & (TweetBotQueue.bot_flag == False))
+            & (TweetBotQueue.bot_flag == False)).order_by(TweetBotQueue.trend_type.asc())
 
         for status in status_list:
-            previous_status = twitter.tweet(status, previous_status)
+            previous_status = twitter.tweet(status, previous_status, first)
+            # print(status.param.body_text)
+            # print(status.handle)
+            first = False
             status.bot_flag = True
             status.save()
 
         last_tweet = TweetBotParams.select().where((TweetBotParams.slug == 'end') &
                                                    (TweetBotParams.lang == language)).get()
         twitter.end_thread(last_tweet, previous_status)
+        print(last_tweet.body_text)
--- twitter.py
+++ twitter.py
@@ -4,11 +4,14 @@ import time
 from datetime import datetime
 
 
-def tweet(status, previous_status):
+def tweet(status, previous_status, first):
     api = twitter_auth.authenticate()
-    status = api.update_status(
-        in_reply_to_status_id=previous_status.id, status=status.param.body_text + " " + get_url(status.handle, status.status_id))
-
+    if first:
+            status = api.update_status(status=status.param.body_text + " " + get_url(status.handle, status.status_id))
+    else:
+        status = api.update_status(
+            in_reply_to_status_id=previous_status.id, status=status.param.body_text + " " + get_url(status.handle, status.status_id))
+            
     return status
 
 def start_thread(param):
