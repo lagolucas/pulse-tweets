7bde130d4fd548880065e22e167feb415efe36be
Lucas Lago <lago@nic.br>
2020-09-19 10:30:32 -0300

    removing commented code

--- tuitar.py
+++ tuitar.py
@@ -10,11 +10,6 @@ if __name__ == '__main__':
         first = True
         previous_status = None
 
-        # first_tweet = TweetBotParams.select().where((TweetBotParams.slug == 'start') 
-        #     & (TweetBotParams.lang == language) 
-        #     & (TweetBotParams.period_day == sys.argv[1])).get()
-        # previous_status = twitter.start_thread(first_tweet)
-
         status_list = TweetBotQueue.select(TweetBotQueue, TweetBotParams).join(TweetBotParams).where(
             (TweetBotQueue.param.lang == language) 
             & (TweetBotParams.period_day == sys.argv[1]) 
@@ -22,13 +17,11 @@ if __name__ == '__main__':
 
         for status in status_list:
             previous_status = twitter.tweet(status, previous_status, first)
-            # print(status.param.body_text)
-            # print(status.handle)
             first = False
             status.bot_flag = True
             status.save()
 
-        last_tweet = TweetBotParams.select().where((TweetBotParams.slug == 'end') &
-                                                   (TweetBotParams.lang == language)).get()
-        twitter.end_thread(last_tweet, previous_status)
-        print(last_tweet.body_text)
+        if (len(status_list) > 0):
+            last_tweet = TweetBotParams.select().where((TweetBotParams.slug == 'end') &
+                                                    (TweetBotParams.lang == language)).get()
+            twitter.end_thread(last_tweet, previous_status)
